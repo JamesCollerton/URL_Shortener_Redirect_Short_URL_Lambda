5013fe050bd43170c00ba36dc87dc0ac25165110
James Collerton <jamescollerton@live.co.uk>
2019-03-31 15:29:01 +0100

    Trying to move code to S3

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,6 +1,8 @@
 module "lambda" {
   source      		   	= "github.com/JamesCollerton/Terraform_Modules//lambda"
-  lambda_function_filename 	= "${var.lambda_function_filename}" 
+#  lambda_function_filename 	= "${var.lambda_function_filename}" 
+  lambda_s3_bucket              = "${var.lambda_s3_bucket}"
+  lambda_s3_key                 = "${var.lambda_s3_key}"
   lambda_function_name 		= "${var.lambda_function_name}" 
   lambda_function_handler 	= "${var.lambda_function_handler}" 
   iam_for_lambda_arn 		= "${var.iam_for_lambda_arn}" 
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -4,6 +4,17 @@ variable "lambda_function_filename" {
   type 		= "string"
 }
 
+variable "lambda_s3_bucket" {
+  description   = "Name of the S3 bucket where we want to store our code"
+  type          = "string"
+}
+
+variable "lambda_s3_key" {
+  default       = "redirectshorturl-0.0.1-SNAPSHOT.jar"
+  description   = "Object key of the S3 bucket where we want to store our code"
+  type          = "string"
+}
+
 variable "lambda_function_name" {
   default 	= "url-shortener-redirect-short-url-lambda"
   description 	= "Name we would like to assign to the lambda function"
