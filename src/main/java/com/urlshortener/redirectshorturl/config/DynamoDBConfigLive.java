62d7ba8056c4d05e3ec8c61f71d2d0c6aed3e708
James Collerton <jamescollerton@live.co.uk>
2019-05-06 11:41:56 +0100

    Updated profiles

--- src/main/java/com/urlshortener/redirectshorturl/config/DynamoDBConfigLive.java
+++ src/main/java/com/urlshortener/redirectshorturl/config/DynamoDBConfigLive.java
@@ -16,7 +16,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Slf4j
-//@Profile("default")
+@Profile("default")
 @Configuration
 @EnableDynamoDBRepositories(basePackageClasses = ShortenedUrlInformationRepository.class)
 public class DynamoDBConfigLive {
